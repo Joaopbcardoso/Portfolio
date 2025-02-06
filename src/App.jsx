f8ce7fb069fd0042cbf1503edc9adcb0f379464d
joaopbcardoso <joao_pb_cardoso@estudante.sesisenai.org.br>
2025-02-06 19:37:40 -0300

    favicon

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import { GlobalStyles } from './styles/GlobalStyles';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
@@ -8,11 +8,30 @@ import Skills from './pages/Skills';
 import Projects from './pages/Projects';
 import Contact from './pages/Contact';
 
+const PageTitleUpdater = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    const routeTitles = {
+      '/': 'Home - My Website',
+      '/about': 'About - My Website',
+      '/skills': 'Skills - My Website',
+      '/projects': 'Projects - My Website',
+      '/contact': 'Contact - My Website',
+    };
+
+    document.title = routeTitles[location.pathname] || 'My Website';
+  }, [location]);
+
+  return null;
+};
+
 function App() {
   return (
     <Router>
       <GlobalStyles />
       <Navbar />
+      <PageTitleUpdater />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
